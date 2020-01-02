8868f8524c9138a0524d0a89ce1cf88b7449fb9a
Alberto <asoler@topdoctors.es>
2020-01-02 14:48:04 +0100

    Changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hi there!</p>
-        <p>Carol guapa!</p>
+        <p>Alberto & Carol</p>
       </header>
     </div>
   );
